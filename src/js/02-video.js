aab75aec0c1096f23d89298c938d55cda46cd694
wordisstuff <wordisstuff@gmail.com>
2023-10-22 15:33:33 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,13 @@ import { throttle } from 'throttle-debounce';
 
 const player = new Player('vimeo-player');
 
-const sturt = player.on('timeupdate', throttle(1000, function (evnt) {
+player.on('timeupdate', throttle(1000, function (evnt) {
     const time = evnt.seconds;
     localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
 }));
 const currentTime = localStorage.getItem('videoplayer-current-time');
 
-if (currentTime) {
-    player.setCurrentTime(JSON.parse(currentTime));
-};
+// if (currentTime) {
+//     player.setCurrentTime(JSON.parse(currentTime));
+// };
 
