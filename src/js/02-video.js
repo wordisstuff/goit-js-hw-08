74ac7c42ca24f573e0e6a70b6115e8e990ff1d3c
wordisstuff <wordisstuff@gmail.com>
2023-10-22 14:32:28 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import { throttle } from 'throttle-debounce';
+
+const player = new Player('vimeo-player');
+player.on('timeupdate', throttle(1000, function (data) {
+    const time = data.seconds;
+    console.log(time);
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+}));
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) {
+    player.setCurrentTime(JSON.parse(currentTime));
+}
 
